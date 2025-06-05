87ba79dfaa0f2a67b6d00a67468885faa511375d
Pedro Silva <pedluc90@gmail.com>
2025-06-05 16:36:42 -0300

    Adicionar classe Validator para validação de transações

---
+++ app/Utils/Validator.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\Utils;
+
+class Validator
+{
+    public static function validarTransacao(array $data): bool
+    {
+        if (!isset($data['id'], $data['valor'], $data['dataHora'])) {
+            return false;
+        }
+
+        if (!self::isUUID($data['id'])) {
+            return false;
+        }
+
+        if (!is_numeric($data['valor']) || (float)$data['valor'] < 0) {
+            return false;
+        }
+
+        if (!self::isISO8601($data['dataHora'])) {
+            return false;
+        }
+
+        $dataHoraTimestamp = strtotime($data['dataHora']);
+        if ($dataHoraTimestamp === false || $dataHoraTimestamp > time()) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private static function isUUID(string $uuid): bool
+    {
+        return preg_match('/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i', $uuid);
+    }
+
+    private static function isISO8601(string $date): bool
+    {
+        $d = \DateTime::createFromFormat(\DateTime::ATOM, $date);
+        return $d && $d->format(\DateTime::ATOM) === $date;
+    }
+}
