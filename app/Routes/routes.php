096c13e94081ff32abd46651bd05358b66d48dea
Pedro Silva <pedluc90@gmail.com>
2025-06-05 16:37:08 -0300

    Implementar conexão com o banco de dados e rota para criação de transações

--- app/Routes/routes.php
+++ app/Routes/routes.php
@@ -1,6 +1,37 @@
 <?php
 use Slim\App;
+use App\Controllers\TransacaoController;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use PDO;
+use Exception;
 
 return function (App $app) {
+
+    $createPdoConnection = function () use ($app) {
+        $config = require __DIR__ . '/../../config/database.php';
+        try {
+            $pdo = new PDO(
+                "mysql:host={$config['host']};dbname={$config['dbname']};charset=utf8mb4",
+                $config['user'],
+                $config['pass'],
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+                ]
+            );
+            return $pdo;
+        } catch (PDOException $e) {
+            error_log("Erro de conexão com o banco de dados: " . $e->getMessage());
+            throw new Exception("Não foi possível conectar ao banco de dados.", 0, $e);
+        }
+    };
+
+    $app->post('/transacao', function (Request $request, Response $response) use ($createPdoConnection) {
+        $db = $createPdoConnection();
+        $controller = new TransacaoController($db);
+        return $controller->criar($request, $response);
+    });
 
 };
